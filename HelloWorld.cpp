35ffd1311fa6ae319a4408d2cd914583e549a8f2
Joren Vrancken <jorenvrancken@gmail.com>
2021-06-18 20:20:56 +0200

    Remove HelloWorld.cpp

--- HelloWorld.cpp
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	MessageBox(NULL, L"World!", L"Hello", MB_OK);
-	return 0;
-}
